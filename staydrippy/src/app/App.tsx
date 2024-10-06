8d27217993fef3d6092204be1ba74b14a3d029e1
beanavas <beanavasaguilera@gmail.com>
2024-10-05 20:15:21 -0400

    website

---
+++ staydrippy/src/app/App.tsx
@@ -0,0 +1,19 @@
+
+"use client"; // Add this at the top
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../../pages';
+
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
